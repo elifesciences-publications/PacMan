441c2befec0666f8444c701a7f119ef66382d45b
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-01-29 20:38:49 +0100

    New subject and adjustments to metascript.

---
+++ analysis/20171211_distcor_func/05_postprocessing/n_06_sh_fsl_copy_mean_trans.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+
+################################################################################
+# The purpose of this script is to copy the mean & tSNR images.                #
+################################################################################
+
+
+#-------------------------------------------------------------------------------
+# Define session IDs & paths:
+
+strPathParent="/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/20171211/nii_distcor/func_reg_tsnr/"
+
+# Functional runs (input & output):
+lstIn=(combined_mean \
+       combined_mean_tSNR)
+
+strPathOutput="/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/20171211/nii_distcor/stat_maps_trans/"
+#-------------------------------------------------------------------------------
+
+
+#-------------------------------------------------------------------------------
+# Copy images:
+
+echo "---Copy images---"
+date
+
+for index01 in ${lstIn[@]}
+do
+
+	strTmpIn="${strPathParent}${index01}.nii.gz"
+	strTmpOut="${strPathOutput}${index01}.nii.gz"
+	echo "------cp ${strTmpIn} ${strTmpOut}"
+	cp ${strTmpIn} ${strTmpOut}
+
+done
+
+date
+echo "done"
+#-------------------------------------------------------------------------------
