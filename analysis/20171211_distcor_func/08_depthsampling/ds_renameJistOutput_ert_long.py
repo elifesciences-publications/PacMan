bca02dd5f4704c88ee7447f503a670d26fdb6b3d
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-02-05 20:33:34 +0100

    Completely bash scripted CBS depth sampling & postprocessing.

--- analysis/20171211_distcor_func/08_depthsampling/ds_renameJistOutput_ert_long.py
+++ analysis/20171211_distcor_func/08_depthsampling/ds_renameJistOutput_ert_long.py
@@ -43,7 +43,8 @@ from os import rename
 dicSubId = {'20171211': 6}
 
 # Paths with files to be renamed (subject ID left open):
-lstPths = ['/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs_distcor/rh_era/pacman_dynamic_long/']  #noqa
+lstPths = ['/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs_distcor/lh_era/pacman_dynamic_long/',
+           '/media/sf_D_DRIVE/MRI_Data_PhD/05_PacMan/{}/cbs_distcor/rh_era/pacman_dynamic_long/']  #noqa
 
 # File type (suffix):
 strFleTpe = '.vtk'
