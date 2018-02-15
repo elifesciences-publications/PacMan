f4a58a2076ef9b43eea7f96210754700583d26e0
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-02-15 20:10:01 +0100

    Consistent metascripting across subjects (automatic spm bias field correction and non-parallelised 2nd level feat).

--- analysis/20171211_distcor_func/01_preprocessing/n_00_metascript.sh
+++ analysis/20171211_distcor_func/01_preprocessing/n_00_metascript.sh
@@ -30,23 +30,19 @@ read -r -s -d $'o'
 date
 
 echo "---Automatic: Reorient & rename images:"
-echo "   > ~/01_preprocessing/n_03_sh_reorient.sh"
 source ${strPathPrnt}01_preprocessing/n_03_sh_reorient.sh
 date
 
 echo "---Automatic: Reverse order of opposite PE images"
-echo "   > ~/01_preprocessing/n_04_inverse_order_func_op"
 python ${strPathPrnt}01_preprocessing/n_04_py_inverse_order_func_op.py
 date
 
 echo "---Automatic: Prepare moco of SE EPI images"
-echo "   > ~/01_preprocessing/n_05b_sh_fsl_preprocessing_1"
 source ${strPathPrnt}01_preprocessing/n_05a_sh_prepare_moco.sh
 source ${strPathPrnt}01_preprocessing/n_05b_sh_prepare_moco.sh
 date
 
 echo "---Automatic: Prepare moco"
-echo "   > ~/01_preprocessing/n_05a_sh_fsl_preprocessing"
 source ${strPathPrnt}01_preprocessing/n_05c_sh_prepare_moco.sh
 date
 
@@ -63,29 +59,24 @@ read -r -s -d $'o'
 date
 
 echo "---Automatic: Run SPM motion correction on functional data"
-echo "   > ~/01_preprocessing/n_06a_spm_create_moco_batch.m"
 matlab -nodisplay -nojvm -nosplash -nodesktop \
   -r "run('/home/john/PhD/GitHub/PacMan/analysis/20171211_distcor_func/01_preprocessing/n_06a_spm_create_moco_batch.m');"
 date
 
 echo "---Automatic: Run SPM motion correction on opposite-phase polarity data"
-echo "   > ~/01_preprocessing/n_06c_spm_create_moco_batch_op.m"
 matlab -nodisplay -nojvm -nosplash -nodesktop \
   -r "run('/home/john/PhD/GitHub/PacMan/analysis/20171211_distcor_func/01_preprocessing/n_06c_spm_create_moco_batch_op.m');"
 date
 
 echo "---Automatic: Copy moco results"
-echo "   > ~/01_preprocessing/n_07a_sh_postprocess_moco.sh"
 source ${strPathPrnt}01_preprocessing/n_07a_sh_postprocess_moco.sh
 date
 
 echo "---Automatic: Copy moco results of SE EPI images"
-echo "   > ~/01_preprocessing/n_07b_sh_postprocess_moco.sh"
 source ${strPathPrnt}01_preprocessing/n_07b_sh_postprocess_moco.sh
 date
 
 echo "---Automatic: Copy moco results of opposite-phase polarity SE EPI images"
-echo "   > ~/01_preprocessing/n_07c_sh_postprocess_moco.sh"
 source ${strPathPrnt}01_preprocessing/n_07c_sh_postprocess_moco.sh
 date
 
@@ -108,93 +99,75 @@ date
 #read -r -s -d $'o'
 
 echo "---Automatic: Calculate fieldmaps"
-echo "   > ~/01_preprocessing/n_08a_sh_fsl_topup.sh"
 source ${strPathPrnt}01_preprocessing/n_08a_sh_fsl_topup.sh
 date
 
 echo "---Automatic: Apply TOPUP on functional data"
-echo "   > ~/01_preprocessing/n_09a_fsl_applytopup.sh"
 source ${strPathPrnt}01_preprocessing/n_09a_fsl_applytopup.sh
 date
 
 echo "---Automatic: Apply TOPUP on SE EPI data"
-echo "   > ~/01_preprocessing/n_09b_fsl_applytopup.sh"
 source ${strPathPrnt}01_preprocessing/n_09b_fsl_applytopup.sh
 date
 
 echo "---Automatic: Create mean undistorted SE EPI image."
-echo "   > ~/01_preprocessing/n_10_sh_mean_se.sh"
 source ${strPathPrnt}01_preprocessing/n_10_sh_mean_se.sh
 date
 
 echo "---Automatic: 1st level FSL FEAT with sustained predictors."
-echo "   > ~/01_preprocessing/n_01_feat_level_1_script_parallel.sh"
 source ${strPathPrnt}02_feat/n_01_feat_level_1_script_parallel.sh
 date
 
 echo "---Automatic: 1st level FSL FEAT with transient predictors."
-echo "   > ~/01_preprocessing/n_02_feat_level_1_script_parallel_trans.sh"
 source ${strPathPrnt}02_feat/n_02_feat_level_1_script_parallel_trans.sh
 date
 
 echo "---Automatic: Calculate tSNR maps."
-echo "   > ~/03_intermediate_steps/n_01_sh_tSNR.sh"
 source ${strPathPrnt}03_intermediate_steps/n_01_sh_tSNR.sh
 date
 
 echo "---Automatic: Update FEAT directories (dummy registration)."
-echo "   > ~/03_intermediate_steps/n_02a_sh_fsl_updatefeatreg.sh"
 source ${strPathPrnt}03_intermediate_steps/n_02a_sh_fsl_updatefeatreg.sh
 date
 
 echo "---Automatic: Update FEAT directories (dummy registration) for transient"
 echo "   predictors."
-echo "   > ~/03_intermediate_steps/n_02b_sh_fsl_updatefeatreg_trans.sh"
 source ${strPathPrnt}03_intermediate_steps/n_02b_sh_fsl_updatefeatreg_trans.sh
 date
 
 echo "---Automatic: Create event related averages."
-echo "   > ~/03_intermediate_steps/n_03a_py_evnt_rltd_avrgs.py"
 python ${strPathPrnt}03_intermediate_steps/n_03a_py_evnt_rltd_avrgs.py
 date
 
 echo "---Automatic: Create event related averages."
-echo "   > ~/03_intermediate_steps/n_03b_py_evnt_rltd_avrgs.py"
 python ${strPathPrnt}03_intermediate_steps/n_03b_py_evnt_rltd_avrgs.py
 date
 
 echo "---Automatic: Create event related averages."
-echo "   > ~/03_intermediate_steps/n_03c_py_evnt_rltd_avrgs.py"
 python ${strPathPrnt}03_intermediate_steps/n_03c_py_evnt_rltd_avrgs.py
 date
 
 echo "---Automatic: Create event related averages."
-echo "   > ~/03_intermediate_steps/n_03d_py_evnt_rltd_avrgs.py"
 python ${strPathPrnt}03_intermediate_steps/n_03d_py_evnt_rltd_avrgs.py
 date
 
 echo "---Automatic: Prepare depth-sampling of event related averages."
-echo "   > ~/03_intermediate_steps/n_04_sh_prepare_era_depthsampling.sh"
 source ${strPathPrnt}03_intermediate_steps/n_04_sh_prepare_era_depthsampling.sh
 date
 
 echo "---Automatic: Calculate spatial correlation."
-echo "   > ~/03_intermediate_steps/n_12_py_spatial_correlation.py"
 python ${strPathPrnt}03_intermediate_steps/n_12_py_spatial_correlation.py
 date
 
 echo "---Automatic: 2nd level FSL FEAT with sustained predictors."
-echo "   > ~/04_feat/n_01_feat_level_2_parallel.sh"
 source ${strPathPrnt}04_feat/n_01_feat_level_2_parallel.sh
 date
 
 echo "---Automatic: 2nd level FSL FEAT with transient predictors."
-echo "   > ~/04_feat/n_02_feat_level_2_parallel_trans.sh"
 source ${strPathPrnt}04_feat/n_02_feat_level_2_parallel_trans.sh
 date
 
 echo "---Automatic: Copy FEAT results."
-echo "   > ~/04_feat/n_0*_sh_fsl_copy_*.sh"
 source ${strPathPrnt}05_postprocessing/n_01_sh_fsl_copy_stats.sh
 source ${strPathPrnt}05_postprocessing/n_02_sh_fsl_copy_stats_trans.sh
 source ${strPathPrnt}05_postprocessing/n_03_sh_fsl_copy_stats_pe.sh
@@ -204,14 +177,11 @@ source ${strPathPrnt}05_postprocessing/n_06_sh_fsl_copy_mean_trans.sh
 date
 
 echo "---Automatic: Upsample FEAT results."
-echo "   > ~/05_postprocessing/n_07_upsample_stats.sh"
 source ${strPathPrnt}05_postprocessing/n_07_upsample_stats.sh
-echo "   > ~/05_postprocessing/n_08_upsample_stats_trans.sh"
 source ${strPathPrnt}05_postprocessing/n_08_upsample_stats_trans.sh
 date
 
 echo "---Automatic: Prepare pRF analysis."
-echo "   > ~/07_pRF/01_py_prepare_prf.py"
 python ${strPathPrnt}07_pRF/01_py_prepare_prf.py
 date
 
@@ -228,30 +198,24 @@ source activate py_main
 date
 
 echo "---Automatic: Upsample pRF results."
-echo "   > ~/07_pRF/03_upsample_retinotopy.sh"
 source ${strPathPrnt}07_pRF/03_upsample_retinotopy.sh
 date
 
 echo "---Automatic: Calculate overlap between voxel pRFs and stimulus."
-echo "   > ~/07_pRF/04_PacMan_pRF_overlap.py"
 python ${strPathPrnt}07_pRF/04_PacMan_pRF_overlap.py
 date
 
 echo "---Automatic: Copy input files for SPM bias field correction."
-echo "   > ~/06_mp2rage/n_01_prepare_spm_bf_correction.sh"
 source ${strPathPrnt}06_mp2rage/n_01_prepare_spm_bf_correction.sh
 date
 
-echo "---Manual: Create and run SPM bias field correction batch."
-echo "   > ~/06_mp2rage/n_01_sh_create_folders.sh"
-echo "   Type 'go' to continue"
-read -r -s -d $'g'
-read -r -s -d $'o'
+echo "---Automatic: SPM bias field correction."
+matlab -nodisplay -nojvm -nosplash -nodesktop \
+  -r "run('/home/john/PhD/GitHub/PacMan/analysis/20171211_distcor_func/06_mp2rage/n_02_spm_bf_correction.m');"
 date
 
 echo "---Automatic: Copy results of SPM bias field correction, and remove"
 echo "   redundant files."
-echo "   > ~/06_mp2rage/n_03_postprocess_spm_bf_correction.sh"
 source ${strPathPrnt}06_mp2rage/n_03_postprocess_spm_bf_correction.sh
 date
 
@@ -264,38 +228,31 @@ read -r -s -d $'o'
 date
 
 echo "---Automatic: Upsample combined mean for MP2RAGE registration."
-echo "   > ~/06_mp2rage/n_05_upsample_mean_epi.sh"
 source ${strPathPrnt}06_mp2rage/n_05_upsample_mean_epi.sh
 date
 
 echo "---Automatic: Prepare MP2RAGE to combined mean registration pipeline."
-echo "   > ~/06_mp2rage/n_06_sh_prepare_reg.sh"
 source ${strPathPrnt}06_mp2rage/n_06_sh_prepare_reg.sh
 date
 
 echo "---Automatic: Register MP2RAGE image to mean EPI"
-echo "   > ~/06_mp2rage/n_07_spm_create_corr_batch_prereg.m"
 matlab -nodisplay -nojvm -nosplash -nodesktop \
   -r "run('/home/john/PhD/GitHub/PacMan/analysis/20171211_distcor_func/06_mp2rage/n_07_spm_create_corr_batch_prereg.m');"
 date
 
 echo "---Automatic: Postprocess SPM registration results."
-echo "   > ~/06_mp2rage/n_08_sh_postprocess_spm_prereg.sh"
 source ${strPathPrnt}06_mp2rage/n_08_sh_postprocess_spm_prereg.sh
 date
 
 echo "---Automatic: Prepare BBR."
-echo "   > ~/06_mp2rage/n_09_py_prepare_bbr.py"
 python ${strPathPrnt}06_mp2rage/n_09_py_prepare_bbr.py
 date
 
 echo "---Automatic: Perform BBR."
-echo "   > ~/06_mp2rage/n_10_sh_bbr.sh"
 source ${strPathPrnt}06_mp2rage/n_10_sh_bbr.sh
 date
 
 echo "---Automatic: Copy BBR results for segmentation."
-echo "   > ~/06_mp2rage/n_11_copy.sh"
 source ${strPathPrnt}06_mp2rage/n_11_copy.sh
 date
 
