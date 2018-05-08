6cdd65995e4c507f9e3f72b656d94b2d2cc8887f
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-05-08 22:22:12 +0200

    Copy sustained and transient feat results.

--- analysis/20180118/05_postprocessing/n_03_sh_fsl_copy_stats_comb.sh
+++ analysis/20180118/05_postprocessing/n_03_sh_fsl_copy_stats_comb.sh
@@ -21,13 +21,14 @@ lstIn=(feat_level_2_Control_Dynamic \
        feat_level_2_Linear)
 
 strPathParent02=".gfeat/cope1.feat/stats/zstat1.nii.gz"
+strPathParent03=".gfeat/cope2.feat/stats/zstat1.nii.gz"
 
 strPathOutput="${pacman_data_path}${pacman_sub_id}/nii/stat_maps_comb/"
 #-------------------------------------------------------------------------------
 
 
 #-------------------------------------------------------------------------------
-# Copy and rename statistical maps:
+# Copy and rename statistical maps - sustained predictor:
 
 echo "---Copy and rename statistical maps---"
 date
@@ -36,7 +37,27 @@ for index01 in ${lstIn[@]}
 do
 
 	strTmpIn="${strPathParent01}${index01}${strPathParent02}"
-	strTmpOut="${strPathOutput}${index01}_zstat1.nii.gz"
+	strTmpOut="${strPathOutput}${index01}_zstat1_sustained.nii.gz"
+	echo "------cp ${strTmpIn} ${strTmpOut}"
+	cp ${strTmpIn} ${strTmpOut}
+
+done
+
+date
+echo "done"
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Copy and rename statistical maps - transient predictor:
+
+echo "---Copy and rename statistical maps---"
+date
+
+for index01 in ${lstIn[@]}
+do
+
+	strTmpIn="${strPathParent01}${index01}${strPathParent03}"
+	strTmpOut="${strPathOutput}${index01}_zstat1_transient.nii.gz"
 	echo "------cp ${strTmpIn} ${strTmpOut}"
 	cp ${strTmpIn} ${strTmpOut}
 
