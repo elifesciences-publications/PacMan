55a89af5e715f9610cfaa66d712d7181b6cf6788
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-05-14 10:56:30 +0200

    Directory for stat maps combined transient & sustained. Autocompleted feat fsf files.

--- analysis/20171204_02/00_get_data/n_01_sh_create_folders.sh
+++ analysis/20171204_02/00_get_data/n_01_sh_create_folders.sh
@@ -136,6 +136,9 @@ then
 	mkdir "${str_nii}/stat_maps_trans"
 	mkdir "${str_nii}/stat_maps_trans_up"
 
+	mkdir "${str_nii}/stat_maps_comb"
+	mkdir "${str_nii}/stat_maps_comb_up"
+
 else
 
 	echo "Analysis directory ${str_nii} already exists."
